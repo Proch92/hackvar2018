588ab868b9ff3564c99bffd5a0f9c983645b7b64
Michele Proverbio <proch92@gmail.com>
2018-05-21 11:52:22 +0200

    listener 0.0.0.0

--- db/restserver.js
+++ db/restserver.js
@@ -19,7 +19,7 @@ var userData_acty = mongoose.model("userData_acty", ({
     n_video 			: 	Number
 }));
 
-mongoose.connect('mongodb://localhost:27017/indilium-db');
+mongoose.connect('mongodb://0.0.0.0:27017/indilium-db');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
